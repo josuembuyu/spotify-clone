217b0d7d260c697050c187c3cd84dc70f24d6817
josuembuyu <josuembuyu94@gmail.com>
2022-02-08 19:16:20 +0100

    Design sidebar component

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Sidebar from "../components/Sidebar"
 export default function Home() {
   return (
     <div className="">
-      <main>
+      <main className="bg-black h-screen overflow-hidden">
         <Sidebar />
-        
+
       </main>
 
       <div>
