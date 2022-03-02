4508be39ed0044296ef52f7c2e53901294847428
josuembuyu <josuembuyu94@gmail.com>
2022-03-02 12:32:28 +0100

    Implementing recoil js

--- components/Center.js
+++ components/Center.js
@@ -1,7 +1,7 @@
-
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { ChevronDownIcon } from "@heroicons/react/outline";
+import { shuffle } from "lodash";
 
 const colors = [
   "from-indigo-500",
@@ -10,27 +10,28 @@ const colors = [
   "from-red-500",
   "from-yellow-500",
   "from-pink-500",
-  "from-purple-500"
+  "from-purple-500",
 ];
 
 function Center() {
   const { data: session } = useSession();
-  const [color, setColor] = useState(null)
+  const [color, setColor] = useState(null);
 
   useEffect(() => {
-    setColor()
-  }, [])
+    setColor(shuffle(colors).pop());
+  }, []);
   return (
     <div className="flex-grow">
-      <header className="absolute top-5 right-0">
+      <header className="absolute top-5 right-10">
         <div className="flex items-center bg-red-300 space-x-2 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
           <img className="rounded-full w-10 h-10" src={session?.user.image} />
           <h2>{session?.user.name}</h2>
           <ChevronDownIcon className="w-5 h-5" />
         </div>
       </header>
-      <section className="flex items-end bg-gradient-to-b to-black space-x-7 from-red-500 h-80 text-white padding-8 w-full">
-          <h1>Hello</h1>
+      <section className={`flex items-end bg-gradient-to-b to-black space-x-7 ${color} h-80 text-white padding-8 w-full`}>
+
+
       </section>
     </div>
   );
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,9 +8,27 @@ import {
 } from "@heroicons/react/outline";
 
 import { signOut, useSession } from "next-auth/react";
+import useSpotify from "../hooks/useSpotify";
+import { useState, useEffect } from "react";
 
 function Sidebar() {
+  const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
+  const [playlists, setPlaylists] = useState([]);
+  const [playListId, setPlayListId] = useState(null);
+
+  console.log("You picked playlist id " + playListId)
+  
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists(session).then((data) => {
+        setPlaylists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
+
+  console.log(playlists)
+
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide">
       <div className="space-y-4">
@@ -48,22 +66,9 @@ function Sidebar() {
         <hr className="border-t-[0.1px] border-gray-900" />
 
         {/* Playlist... */}
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        {playlists.map((playlist) => (
+            <p onClick={() => setPlayListId(playlist.id)} key={playlist.id} className="cursor-pointer hover:text-white">{playlist.name}</p>
+        ))}
       </div>
     </div>
   );
